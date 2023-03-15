97564a863af4d44d509bffbcb618e94c3fa1542d
Gabriel Akinmoyero <akinmoyerogabriel@gmail.com>
2023-03-15 21:03:28 +0100

    Add react, node and docker files

---
+++ microservices/backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const data = [
+  { id: 1,  name: 'Apple' },
+  { id: 2,  name: 'Banana' },
+  { id: 3,  name: 'Orange' },
+  { id: 4,  name: 'Grapefruit' },
+  { id: 5,  name: 'Pineapple' },
+  { id: 6,  name: 'Mango' },
+  { id: 7,  name: 'Kiwi' },
+  { id: 8,  name: 'Pear' },
+  { id: 9,  name: 'Peach' },
+  { id: 10, name: 'Plum' },
+];
+
+app.get('/api/data', (req, res) => {
+  res.json(data);
+});
+
+app.listen(8000, () => {
+  console.log(`Backend microservice running on ${process.env.BACKEND_URL}`);
+});
